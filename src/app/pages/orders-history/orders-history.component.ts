41473ae4a8e23d3c26bcd7194af90ce157cab880
MahmoudFadl <mfadl4201@gmail.com>
2024-06-30 22:34:54 +0300

    feat: order list

--- src/app/pages/orders-history/orders-history.component.ts
+++ src/app/pages/orders-history/orders-history.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-orders-history',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, TableModule, ButtonModule, CardModule,CommonModule],
   templateUrl: './orders-history.component.html',
-  styleUrl: './orders-history.component.scss'
+  styleUrl: './orders-history.component.scss',
 })
 export class OrdersHistoryComponent {
-
+  orders!:any[]
 }
