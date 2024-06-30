346bef91af8529dfe7335079776d0c10139cd16e
MahmoudFadl <mfadl4201@gmail.com>
2024-06-30 18:34:56 +0300

    feat: menus list

--- src/app/pages/menus/menus.component.ts
+++ src/app/pages/menus/menus.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-menus',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, TableModule, ButtonModule, CardModule],
   templateUrl: './menus.component.html',
-  styleUrl: './menus.component.scss'
+  styleUrl: './menus.component.scss',
 })
 export class MenusComponent {
-
+  menus: any[] = [
+    {
+      id: 1,
+      name: 'name',
+    },
+  ];
 }
